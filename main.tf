1f40ea06f60c8919ac093eccdb535fcbf79b01fc
pelvis56 <106154146+pelvis56@users.noreply.github.com>
2022-06-02 17:17:09 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -302,6 +302,7 @@ resource "azurerm_network_interface" "vm" {
   tags = var.tags
 }
 
+
 resource "azurerm_network_interface_security_group_association" "test" {
   count                     = var.nb_instances
   network_interface_id      = azurerm_network_interface.vm[count.index].id
