a168d76908ce9ab422162d466908da71ddfdc25f
pelvis56 <stephane.torlet@dxc.com>
2022-06-04 12:06:42 +0200

    Update main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -12,7 +12,7 @@ data "azurerm_subnet" "subnet" {
 
 module "windowsservers" {
   source              			= "pelvis56/dxc-test/azurerm"
-  version				= "lastest"
+  version				= "latest"
  
   resource_group_name 			= "${data.azurerm_resource_group.dxc.name}"
   vm_hostname         			= "<%=instance.name%>" // line can be removed if only one VM module per resource group
