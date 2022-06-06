1cff2984f05eba4d6b14e96c5f9150191e604da4
pelvis56 <stephane.torlet@dxc.com>
2022-06-06 19:04:44 +0200

    Provider Ref removed

---
+++ cloudbolt/compute/main.tf
@@ -0,0 +1,49 @@
+#refer to a subnet
+data "azurerm_subnet" "subnet" {
+  name                 = split("/", var.subnet_name)[1]
+  virtual_network_name = split("/", var.subnet_name)[0]
+  resource_group_name  = "sdc3-net-shared-rg"
+}
+
+module "windowsservers" {
+  source              			= "pelvis56/dxc-test/azurerm"
+ 
+  resource_group_name 			= var.resource_group_name
+  vm_hostname         			= var.vm_hostname // line can be removed if only one VM module per resource group
+  delete_data_disks_on_termination 	= true
+  delete_os_disk_on_termination 	= true
+  is_nsg             			= false
+  is_windows_image              	= true
+  admin_password                	= "ComplxP@ssw0rd!"
+  allocation_method             	= "Static"
+  public_ip_sku                 	= "Standard"
+  public_ip_dns                 	= ["test-pip"]
+  nb_public_ip                  	= 0
+  vm_os_publisher               	= "MicrosoftWindowsServer"
+  vm_os_offer                   	= "WindowsServer"
+  vm_os_sku                     	= "2012-R2-Datacenter"
+  vm_size                       	= var.vm_size
+  vnet_subnet_id      			= "${data.azurerm_subnet.subnet.id}"
+  enable_accelerated_networking 	= true
+  license_type                  	= "Windows_Client"
+  identity_type                 	= "SystemAssigned" // can be empty, SystemAssigned or UserAssigned
+
+  nb_data_disk                     	= 2
+  data_disk_size_gb                	= 64
+  data_sa_type                     	= "Premium_LRS"
+
+  extra_disks = [
+    {
+      size = 50
+      name = "extraDisk1"
+    },
+    {
+      size = 200
+      name = "extraDisk2"
+    }
+  ] 
+}
+
+output "windows_vm_hostname" {
+  value = module.windowsservers.network_interface_private_ip
+}
---
+++ cloudbolt/compute/variables.tf
@@ -0,0 +1,47 @@
+variable "subscription_id" {
+  description = "Customer Subscription ID"
+  default = ""
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Customer Tenant ID"
+  default = ""
+  type        = string
+}
+
+variable "client_id" {
+  description = "Customer Client SPN ID"
+  default = ""
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Client SP secret key"
+  default = ""
+  type        = string
+}
+
+variable "vm_hostname" {
+  description = "Azure VM size"
+  type        = string
+}
+
+variable "vm_size" {
+  description = "Azure VM size"
+  type        = string
+}
+
+variable "subnet_name" {
+  description = "Azure Subnet definition"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Azure Resource Group definition"
+  type        = string
+}
+
+
+
+
