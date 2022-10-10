4d1c84ad10f801596a6282e1103fb0538281c393
vldudnic <45792749+vldudnic@users.noreply.github.com>
2022-10-10 18:39:29 +0200

    Update main.tf

--- cloudbolt/modules/compute/virtual_machine/main.tf
+++ cloudbolt/modules/compute/virtual_machine/main.tf
@@ -6,7 +6,7 @@ data "azurerm_subnet" "subnet" {
 }
 
 module "linuxservers" {
-  source              			= "../../../../cores/compute/virtual_machine"
+  source              			= "cloudbolt/cores/compute/virtual_machine"
  
   resource_group_name 			= var.resource_group_name
   vm_hostname         			= var.vm_hostname // line can be removed if only one VM module per resource group
