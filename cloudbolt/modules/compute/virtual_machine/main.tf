816c55ed3742a6e5308927fbbf68dec6af48a961
vldudnic <45792749+vldudnic@users.noreply.github.com>
2022-10-10 16:07:55 +0200

    Update main.tf

--- cloudbolt/modules/compute/virtual_machine/main.tf
+++ cloudbolt/modules/compute/virtual_machine/main.tf
@@ -2,10 +2,10 @@
 data "azurerm_subnet" "subnet" {
   name                 = split("/", var.subnet_name)[1]
   virtual_network_name = split("/", var.subnet_name)[0]
-  resource_group_name  = "sdc3-net-shared-rg"
+  resource_group_name  = "sdc3-net-vd-rg"
 }
 
-module "windowsservers" {
+module "linuxservers" {
   source              			= "../../../cores/compute/virtual_machine"
  
   resource_group_name 			= var.resource_group_name
@@ -13,37 +13,37 @@ module "windowsservers" {
   delete_data_disks_on_termination 	= true
   delete_os_disk_on_termination 	= true
   is_nsg             			= false
-  is_windows_image              	= true
+  is_windows_image              	= false
   admin_password                	= "ComplxP@ssw0rd!"
   allocation_method             	= "Static"
   public_ip_sku                 	= "Standard"
   public_ip_dns                 	= ["test-pip"]
   nb_public_ip                  	= 0
-  vm_os_publisher               	= "MicrosoftWindowsServer"
-  vm_os_offer                   	= "WindowsServer"
-  vm_os_sku                     	= "2012-R2-Datacenter"
+  vm_os_publisher               	= "Ubuntu"
+  vm_os_offer                   	= ""
+  vm_os_sku                     	= ""
   vm_size                       	= var.vm_size
   vnet_subnet_id      			= "${data.azurerm_subnet.subnet.id}"
   enable_accelerated_networking 	= true
-  license_type                  	= "Windows_Client"
+  license_type                  	= ""
   identity_type                 	= "SystemAssigned" // can be empty, SystemAssigned or UserAssigned
 
-  nb_data_disk                     	= 2
+  nb_data_disk                     	= 1
   data_disk_size_gb                	= 64
-  data_sa_type                     	= "Premium_LRS"
+  data_sa_type                     	= "Standard_LRS"
 
-  extra_disks = [
-    {
-      size = 50
-      name = "extraDisk1"
-    },
-    {
-      size = 200
-      name = "extraDisk2"
-    }
-  ] 
+#  extra_disks = [
+#    {
+#      size = 50
+#      name = "extraDisk1"
+#    },
+#    {
+#      size = 200
+#      name = "extraDisk2"
+#    }
+#  ] 
 }
 
-output "windows_vm_hostname" {
-  value = module.windowsservers.network_interface_private_ip
+output "linux_vm_hostname" {
+  value = module.linuxserver.network_interface_private_ip
 }
